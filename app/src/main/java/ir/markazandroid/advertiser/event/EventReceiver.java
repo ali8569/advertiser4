0511f887e78921c0a23ef1d4e2b696d591e49dbb
ali8569 <wehatexboc360@icloud.com>
2019-05-13 16:02:14 +0430

    support for mirror events

--- app/src/main/java/ir/markazandroid/advertiser/event/EventReceiver.java
+++ app/src/main/java/ir/markazandroid/advertiser/event/EventReceiver.java
@@ -13,24 +13,47 @@ public class EventReceiver extends BroadcastReceiver {
 
     private Context context;
 
-    public static final String EVENT_TYPE_DEVICE_AUTHENTICATED="EVENT_TYPE_DEVICE_AUTHENTICATED";
-    public static final String EVENT_TYPE_NAME="ir.markazandroid.police.event.BaseEvent.EVENT_TYPE_NAME";
+    public static final String EVENT_TYPE_DEVICE_AUTHENTICATED = "EVENT_TYPE_DEVICE_AUTHENTICATED";
+    public static final String EVENT_TYPE_NAME = "ir.markazandroid.police.event.BaseEvent.EVENT_TYPE_NAME";
+
+    public static final String EVENT_TYPE_MIRROR_BLOCK = "EVENT_TYPE_MIRROR_BLOCK";
+    public static final String EVENT_TYPE_MIRROR_UNBLOCK = "EVENT_TYPE_MIRROR_UNBLOCK";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        this.context=context;
-        Log.e("Event",intent.toString());
-        switch (intent.getStringExtra(EVENT_TYPE_NAME)){
+        this.context = context;
+        Log.e("Event", intent.toString());
+        switch (intent.getStringExtra(EVENT_TYPE_NAME)) {
             case EVENT_TYPE_DEVICE_AUTHENTICATED:
-                onDeviceAuthenticatedEvent();break;
+                onDeviceAuthenticatedEvent();
+                break;
+
+            case EVENT_TYPE_MIRROR_BLOCK:
+                onMirrorBlockEvent();
+                break;
+
+            case EVENT_TYPE_MIRROR_UNBLOCK:
+                onMirrorUnBlockEvent();
+                break;
         }
     }
 
-    private void onDeviceAuthenticatedEvent(){
+    private void onDeviceAuthenticatedEvent() {
         getSignalManager().sendMainSignal(new Signal(Signal.SIGNAL_DEVICE_AUTHENTICATED));
     }
 
-    private SignalManager getSignalManager(){
-        return ((AdvertiserApplication)context.getApplicationContext()).getSignalManager();
+    private void onMirrorBlockEvent() {
+        Signal signal = new Signal("screen block", Signal.SIGNAL_SCREEN_BLOCK);
+        getSignalManager().sendMainSignal(signal);
     }
+
+    private void onMirrorUnBlockEvent() {
+        Signal signal = new Signal("screen unblock", Signal.SIGNAL_SCREEN_UNBLOCK);
+        getSignalManager().sendMainSignal(signal);
+    }
+
+    private SignalManager getSignalManager() {
+        return ((AdvertiserApplication) context.getApplicationContext()).getSignalManager();
+    }
+
 }
